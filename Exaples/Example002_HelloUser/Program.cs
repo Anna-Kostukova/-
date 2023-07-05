8752e8878bfea4ff42aea0efe33980ea12e191e8
«Anna» <anna.kostukova@gmail.com>
2023-07-05 17:11:10 +0500

    user

---
+++ Exaples/Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// вывести приветствие для пользователя
+
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine(); // возьми то, что справа и положи в то, что слева
+Console.WriteLine("Привет, ");
+Console.Write(username);
