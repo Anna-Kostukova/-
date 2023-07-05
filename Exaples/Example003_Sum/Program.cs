c4166b1a2aae4bcb912f976bb71d319b95b76fa8
«Anna» <anna.kostukova@gmail.com>
2023-07-05 22:51:42 +0500

    sum

--- Exaples/Example003_Sum/Program.cs
+++ Exaples/Example003_Sum/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Вывести сумму двух чисел
-int numberA = 32; //вводим целое число (int)
-int numberB = 50;
+int numberA = new Random().Next(1,10); //вводим целое число (int) / случайное число от 1 до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int result = numberA + numberB; // сохранение результатов в промежуточную переменную
 Console.WriteLine(numberA + numberB);
 
