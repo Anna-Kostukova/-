127802a1211808dd491e9a1edea5716f235e1893
«Anna» <anna.kostukova@gmail.com>
2023-07-05 17:23:17 +0500

    сумма двух чисел

---
+++ Exaples/Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿// Вывести сумму двух чисел
+int numberA = 32; //вводим целое число (int)
+int numberB = 50;
+int result = numberA + numberB; // сохранение результатов в промежуточную переменную
+Console.WriteLine(numberA + numberB);
+
