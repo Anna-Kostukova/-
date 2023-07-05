f9dbd93049d82b6a381c2eb7f0c67bd8a57536cc
«Anna» <anna.kostukova@gmail.com>
2023-07-05 22:52:48 +0500

    div

--- Exaples/Exemple004_Div/Program.cs
+++ Exaples/Exemple004_Div/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Вывести на экран частное двух чисел
-double numberA = 10;
+double numberA = 10; // можем выбирать и дробные числа (double)
 double numberB = 3;
 double result = numberA / numberB;
 Console.WriteLine(numberA / numberB);
