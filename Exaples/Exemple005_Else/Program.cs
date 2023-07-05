bf57f1b3baa0719aceb07318264d593a94a62491
«Anna» <anna.kostukova@gmail.com>
2023-07-05 20:40:58 +0500

    Маша

---
+++ Exaples/Exemple005_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿// Если пользователя зовут, например, Маша, нужно поприветствовать его особенно
+Console.WriteLine("Введите имя пользователя: ");
+string usernume = Console.ReadLine();
+
+if(usernume.ToLower() == "маша")   //если введенное имя совпадает с Маша (ToLower - любыми буквами)
+{
+    Console.WriteLine("Ура, это же МАША!"); 
+}
+    else                   // иначе
+{
+    Console.Write("Привет, ");   
+    Console.WriteLine(usernume);
+}    
