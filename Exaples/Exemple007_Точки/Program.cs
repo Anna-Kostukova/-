884eac6961be099a360162e90e333d5c6bb625c8
«Anna» <anna.kostukova@gmail.com>
2023-07-05 22:48:49 +0500

    спать

---
+++ Exaples/Exemple007_Точки/Program.cs
@@ -0,0 +1,28 @@
+﻿// Определить три точки 
+// Выбрать две любых
+// Найти середину
+// Поставить точку
+// Выбрать случайную вершину треугольника
+// Соединить ее с полученной на 4 шаге точкой
+// Перейти к шагу 3
+// Найти Шаги 3-7 повторить 9, 28, 31, 500, 100  раз;
+
+Console.Clear();
+// Console.SetCursorPosition(10, 4);  // расположение точки на консоли
+// Console.WriteLine("+");  // рисовать будем крестиком
+
+int xa = 1;  // определить три точки
+int xb = 1;
+int yb = 10;
+int ya = 1;              
+int xc = 20;
+int yc = 10;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
