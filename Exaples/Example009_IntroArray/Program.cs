ff52de70333f08aa2d59494674fe0070ffd9e69c
«Anna» <anna.kostukova@gmail.com>
2023-07-07 17:16:56 +0500

    array

---
+++ Exaples/Example009_IntroArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// Ищем максимум из 9 чисел - функция array
+
+int Max(int arg1, int arg2, int arg3) // функция выбора из трех значений максимальное
+{
+    int result = arg1 ;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+            //  0  1  2  3  4   5  6  7  8
+int[] array = {12,52,37,34,98775,666,73,85,229}; // массив из 9 чисел
+ 
+int max = Max(
+     Max(array[0], array[1], array[2]), //  по три числа сравниваем 
+     Max(array[3], array[4], array[5]),
+     Max(array[6], array[7], array[8])      
+);
+
+Console.WriteLine(max);
+
