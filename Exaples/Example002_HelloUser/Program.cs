06bc5d8afcfefff3e837eee72666165858c2ea26
«Anna» <anna.kostukova@gmail.com>
2023-07-12 17:10:54 +0500

    user_добавила еще комментарий

--- Exaples/Example002_HelloUser/Program.cs
+++ Exaples/Example002_HelloUser/Program.cs
@@ -1,6 +1,6 @@
 ﻿// вывести приветствие для пользователя
 
 Console.Write("Введите ваше имя ");
-string username = Console.ReadLine(); // возьми то, что справа и положи в то, что слева
+string username = Console.ReadLine(); // возьми то, что справа и положи в то, что слева "=", переменная()
 Console.WriteLine("Привет, ");
 Console.Write(username); // выводим текст в одну строку "Привет, введенное имя"
