6f087f59cc8f631db1d499b0882fe5839ce35766
«Anna» <anna.kostukova@gmail.com>
2023-07-12 17:37:24 +0500

    цикл_дописала комментарии

--- Exaples/Example008_Цикл/Program.cs
+++ Exaples/Example008_Цикл/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Ищем максимум из 9 чисел
 
-int Max(int arg1, int arg2, int arg3) // функция выбора из трех значений максимальное
+int Max(int arg1, int arg2, int arg3) // функция(метод) выбора из трех значений максимальное
 {
     int result = arg1 ;
     if(arg2 > result) result = arg2;
@@ -17,11 +17,12 @@ int a3 = 45;
 int b3 = 32;
 int c3 = 8;
 
-int max1 = Max(a1, b1, c1); 
+int max1 = Max(a1, b1, c1); // результатом работы функции мах будет являться 
+                            //максимальное из трех чисел (первой тройки) 
 int max2 = Max(a2, b2, c2);
 int max3 = Max(a3, b3, c3);
 
-int max = Max(max1, max2, max3); 
+int max = Max(max1, max2, max3); // выбираем теперь мах из максимальных
 
 Console.WriteLine(max);
 
