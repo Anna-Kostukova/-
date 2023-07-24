16581e75807398bc0642ea72ec2f11ab89f6b990
«Anna» <anna.kostukova@gmail.com>
2023-07-24 22:53:09 +0500

    факториал_доработать

---
+++ Exaples/Exemple017_Факториал/Program.cs
@@ -0,0 +1,15 @@
+﻿// 5! = 5 х 4 х 3 х 2 х 1
+// 5! = 5 х 4!
+// 4! = 4 х 3!
+// 3! = 3 х 2!
+
+double Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
